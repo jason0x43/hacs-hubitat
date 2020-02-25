f0fbd99949639f0133c145b472d6f543bb4c205c
Troy Stephens <stephens.ta@gmail.com>
2020-02-24 19:50:37 -0500

    Changed fan component to match newer code

--- custom_components/hubitat/fan.py
+++ custom_components/hubitat/fan.py
@@ -1,18 +1,18 @@
-"""Support for Hubitat switches."""
+"""Support for Hubitat fans."""
 
 from logging import getLogger
 import re
 from typing import Any, List, Optional
 
 from hubitatmaker import (
+	ATTR_SPEED,
     CAP_FAN_CONTROL,
     CMD_SET_LEVEL,
     CMD_OFF, 
     CMD_ON, 
     CMD_CYCLE_SPEED, 
     CMD_SET_SPEED,
-    ATTR_SPEED, 
-    Hub as HubitatHub
+    Device
 )
 
 from homeassistant.components.fan import (
@@ -22,12 +22,10 @@ from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 from homeassistant.util import color as color_util
 
-from .const import DOMAIN
-from .device import HubitatDevice
-
+from .device import Hub, HubitatEntity, get_hub
 _LOGGER = getLogger(__name__)
 
-class HubitatFan(HubitatDevice, FanEntity):
+class HubitatFan(HubitatEntity, FanEntity):
     """Representation of a Hubitat fan."""
 
     @property
@@ -50,7 +48,7 @@ class HubitatFan(HubitatDevice, FanEntity):
 
     @property
     def speed_list(self) -> str:
-        """Return the list of speeds for this fan."""
+        """Return the list of speeds for this fan. (MakerAPI isn't sending the list of speeds)"""
         return ["off", "low", "medium-low", "medium", "high", "auto"]
 
     async def async_turn_on(self, **kwargs: Any) -> None:
@@ -69,18 +67,17 @@ class HubitatFan(HubitatDevice, FanEntity):
 
 
 def is_fan(device) -> bool:
-    """Return True if device looks like a switch."""
-    if CAP_FAN_CONTROL in device["capabilities"]:
-        return True
-    return False
+    """Return True if device looks like a fan."""
+    return  CAP_FAN_CONTROL in device["capabilities"]
 
 
 async def async_setup_entry(
     hass: HomeAssistant, entry: ConfigEntry, async_add_entities,
 ) -> None:
     """Initialize fan devices."""
-    hub: HubitatHub = hass.data[DOMAIN][entry.entry_id].hub
-    fan_devs = [d for d in hub.devices if is_fan(d)]
+    hub = get_hub(hass, entry.entry_id)
+    devices = hub.devices
+    fan_devs = [d for d in devices if is_fan(d)]
     fans: List[HubitatFan] = []
     for f in fan_devs:
         fans.append(HubitatFan(hub=hub, device=f))
