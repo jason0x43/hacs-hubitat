0948dabf35783487d80991f7fb4e9cbbd5e66335
Jason Cheatham <jason@jasoncheatham.com>
2020-12-28 20:14:08 -0500

    Add copy script for dev

--- scripts/__init__.py
+++ scripts/__init__.py
@@ -2,7 +2,10 @@ from subprocess import call, check_output
 
 from hubitatmaker import __version__
 from sys import exit
-from shutil import rmtree
+from shutil import copy as copyfile, rmtree
+from os import chdir, environ
+from os.path import join
+from pathlib import Path
 import toml
 
 
@@ -14,3 +17,27 @@ def init() -> None:
 def test() -> None:
     call("pyright custom_components/hubitat", shell=True)
     call("poetry run pytest", shell=True)
+
+
+def copy() -> None:
+    if "HASS_CONFIG_PATH" not in environ:
+        return
+    dst = Path(environ["HASS_CONFIG_PATH"])
+    src = Path(__file__).parent.parent
+    chdir(src)
+
+    print(f"Copying updated files to {dst}")
+
+    files = (
+        sorted(Path(".").glob("custom_components/**/*.py"))
+        + sorted(Path(".").glob("custom_components/**/*.json"))
+        + sorted(Path(".").glob("custom_components/**/*.yaml"))
+    )
+
+    for f in files:
+        target = dst.joinpath(f)
+        src_data = f.read_text()
+        target_data = target.read_text()
+        if src_data != target_data:
+            copyfile(f, target)
+            print(f"updated {f}")
