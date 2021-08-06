jason0x43/hacs-hubitat

commit ac6c9e8c2b1a6cdf70f76144fd248d634fde4048

Add HSM and mode sensors and services
- Support HSM with a sensor and a set_hsm service
- Support hub mode with a sensor and a set_hub_mode service
- In HA 2021.7+, also add a select entity for the hub mode

resolves #42
resolves #35

custom_components/hubitat/device.py
@@ -45,6 +45,9 @@ _LOGGER = getLogger(__name__)
 
 Listener = Callable[[Event], None]
 
+HUB_DEVICE_NAME = "Hub"
+HUB_NAME = "Hubitat Elevation"
+
 
 class Hub:
     """Representation of a Hubitat hub."""
@@ -74,6 +77,8 @@ class Hub:
             self._hub_entity_id = "hubitat.hub"
         else:
             self._hub_entity_id = f"hubitat.hub_{index}"
+
+        self._hub_device_listeners: List[Listener] = []
 
         self.unsub_config_listener = entry.add_update_listener(_update_entry)
 
@@ -123,6 +128,31 @@ class Hub:
         return self._hub.event_url
 
     @property
+    def mode(self) -> Optional[str]:
+        """Return the current mode of this hub."""
+        return self._hub.mode
+
+    @property
+    def modes(self) -> Optional[List[str]]:
+        """Return the available modes of this hub."""
+        return self._hub.modes
+
+    @property
+    def mode_supported(self) -> Optional[bool]:
+        """Return true if this hub supports mode setting and status."""
+        return self._hub.mode_supported
+
+    @property
+    def hsm_status(self) -> Optional[str]:
+        """Return the current HSM status of this hub."""
+        return self._hub.hsm_status
+
+    @property
+    def hsm_supported(self) -> Optional[bool]:
+        """Return true if this hub supports HSM setting and status."""
+        return self._hub.hsm_supported
+
+    @property
     def token(self) -> str:
         """The token used to access the Maker API."""
         return cast(str, self.config_entry.data.get(CONF_ACCESS_TOKEN))
@@ -134,7 +164,10 @@ class Hub:
 
     def add_device_listener(self, device_id: str, listener: Listener) -> None:
         """Add a listener for events for a specific device."""
-        self._hub.add_device_listener(device_id, listener)
+        if device_id == self.id:
+            self._hub_device_listeners.append(listener)
+        else:
+            self._hub.add_device_listener(device_id, listener)
 
     def add_entities(self, entities: Sequence["HubitatEntity"]) -> None:
         """Add entities to this hub."""
@@ -147,6 +180,17 @@ class Hub:
     def remove_device_listeners(self, device_id: str) -> None:
         """Remove all listeners for a specific device."""
         self._hub.remove_device_listeners(device_id)
+        self._hub_device_listeners = []
+
+    async def set_mode(self, mode: str) -> None:
+        """Set the hub mode"""
+        _LOGGER.debug("Setting hub mode to %s", mode)
+        return await self._hub.set_mode(mode)
+
+    async def set_hsm(self, mode: str) -> None:
+        """Set the hub HSM"""
+        _LOGGER.debug("Setting hub HSM to %s", mode)
+        return await self._hub.set_hsm(mode)
 
     def set_temperature_unit(self, temp_unit: str) -> None:
         """Set the hub's temperature units."""
@@ -187,6 +231,30 @@ class Hub:
         hass = self.hass
         config_entry = self.config_entry
 
+        # setup proxy Device representing the hub that can be used for linked
+        # entities
+        self.device = Device(
+            {
+                "id": self.id,
+                "label": HUB_DEVICE_NAME,
+                "name": HUB_DEVICE_NAME,
+                "attributes": [
+                    {
+                        "name": "mode",
+                        "currentValue": None,
+                        "dataType": "ENUM",
+                    },
+                    {
+                        "name": "hsm_status",
+                        "currentValue": None,
+                        "dataType": "ENUM",
+                    },
+                ],
+                "capabilities": [],
+                "commands": [],
+            }
+        )
+
         for platform in PLATFORMS:
             hass.async_create_task(
                 hass.config_entries.async_forward_entry_setup(config_entry, platform)
@@ -206,6 +274,28 @@ class Hub:
             },
         )
 
+        if self.mode_supported:
+
+            def handle_mode_event(event: Event):
+                self.device.update_attr("mode", cast(str, event.value))
+                for listener in self._hub_device_listeners:
+                    listener(event)
+
+            self._hub.add_mode_listener(handle_mode_event)
+            if self.mode:
+                self.device.update_attr("mode", self.mode)
+
+        if self.hsm_supported:
+
+            def handle_hsm_status_event(event: Event):
+                self.device.update_attr("hsm_status", cast(str, event.value))
+                for listener in self._hub_device_listeners:
+                    listener(event)
+
+            self._hub.add_hsm_listener(handle_hsm_status_event)
+            if self.hsm_status:
+                self.device.update_attr("hsm_status", self.hsm_status)
+
         return True
 
     async def async_update_device_registry(self) -> None:
@@ -216,7 +306,7 @@ class Hub:
             connections={(device_registry.CONNECTION_NETWORK_MAC, self._hub.mac)},
             identifiers={(DOMAIN, self.id)},
             manufacturer="Hubitat",
-            name="Hubitat Elevation",
+            name=HUB_NAME,
         )
 
     @staticmethod
@@ -308,7 +398,7 @@ class HubitatBase:
     def __init__(self, hub: Hub, device: Device, temp: Optional[bool] = False) -> None:
         """Initialize a device."""
         self._hub = hub
-        self._device: Device = device
+        self._device = device
         self._id = f"{get_token_hash(hub.token)}::{self._device.id}"
         self._old_ids = [
             f"{self._hub.host}::{self._hub.app_id}::{self._device.id}",
@@ -329,13 +419,18 @@ class HubitatBase:
     @property
     def device_info(self) -> Dict[str, Any]:
         """Return the device info."""
-        return {
+        info: Dict[str, Any] = {
             "identifiers": {(DOMAIN, self.device_id)},
-            "name": self._device.name,
-            "manufacturer": "Hubitat",
-            "model": self.type,
-            "via_device": (DOMAIN, self._hub.id),
         }
+
+        # if this entities device isn't the hub, link it to the hub
+        if self.device_id != self._hub.id:
+            info["name"] = self.name
+            info["via_device"] = ((DOMAIN, self._hub.id),)
+            info["model"] = self.type
+            info["manufacturer"] = "Hubitat"
+
+        return info
 
     @property
     def old_unique_ids(self) -> List[str]:

custom_components/hubitat/const.py
@@ -12,6 +12,14 @@ from hubitatmaker import (
     CAP_PUSHABLE_BUTTON,
 )
 
+# select entities aren't supported in HA < 2021.7
+try:
+    import homeassistant.components.select as _  # noqa: F401
+
+    has_select = True
+except Exception:
+    has_select = False
+
 DOMAIN = "hubitat"
 
 ATTR_ARGUMENTS = "args"
@@ -27,10 +35,12 @@ ATTR_HA_DEVICE_ID = "ha_device_id"
 ATTR_DOUBLE_TAPPED = "double_tapped"
 ATTR_ENTRY_DELAY = "entry_delay"
 ATTR_EXIT_DELAY = "exit_delay"
+ATTR_HSM_STATUS = "hsm_status"
 ATTR_HUB = "hub"
 ATTR_LAST_CODE_NAME = "last_code_name"
 ATTR_LENGTH = "length"
 ATTR_MAX_CODES = "max_codes"
+ATTR_MODE = "mode"
 ATTR_NAME = "name"
 ATTR_POSITION = "position"
 
@@ -53,6 +63,9 @@ CONF_UNLOCKED_WITH_CODE = "code_name"
 CONF_SUBTYPE = "subtype"
 CONF_VALUE = "value"
 
+DEVICE_TYPE_HUB_MODE = "hub_mode"
+DEVICE_TYPE_HUB_HSM_STATUS = "hub_hsm_status"
+
 ICON_ALARM = "mdi:alarm-bell"
 
 PLATFORMS = [
@@ -67,6 +80,9 @@ PLATFORMS = [
     "fan",
 ]
 
+if has_select:
+    PLATFORMS.append("select")
+
 SERVICE_CLEAR_CODE = "clear_code"
 SERVICE_SEND_COMMAND = "send_command"
 SERVICE_SET_CODE = "set_code"
@@ -75,6 +91,8 @@ SERVICE_SET_ENTRY_DELAY = "set_entry_delay"
 SERVICE_SET_EXIT_DELAY = "set_exit_delay"
 SERVICE_ALARM_SIREN_ON = "alarm_siren_on"
 SERVICE_ALARM_STROBE_ON = "alarm_strobe_on"
+SERVICE_SET_HSM = "set_hsm"
+SERVICE_SET_HUB_MODE = "set_hub_mode"
 
 STEP_USER = "user"
 STEP_REMOVE_DEVICES = "remove_devices"

custom_components/hubitat/select.py
@@ -1,0 +1,71 @@
+from typing import Any, List, Optional, Union
+
+from custom_components.hubitat.device import HubitatEntity, get_hub
+from custom_components.hubitat.types import EntityAdder
+
+from homeassistant.components.select import SelectEntity
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
+
+from .const import ATTR_MODE, DEVICE_TYPE_HUB_MODE
+
+
+class HubitatSelect(HubitatEntity, SelectEntity):
+    _attribute: str
+    _options: List[str]
+    _device_class: str
+
+    @property
+    def device_class(self) -> Optional[str]:
+        """Return this select's device class."""
+        return self._device_class
+
+    @property
+    def name(self) -> str:
+        """Return this select's display name."""
+        return f"{super().name} {self._attribute}"
+
+    @property
+    def current_option(self) -> Union[str, None]:
+        """Return this select's current state."""
+        return self.get_str_attr(self._attribute)
+
+    @property
+    def unique_id(self) -> str:
+        """Return a unique ID for this sensor."""
+        return f"{super().unique_id}::sensor::{self._attribute}"
+
+    @property
+    def options(self) -> List[str]:
+        return self._options or []
+
+
+class HubitatModeSelect(HubitatSelect):
+    def __init__(self, *args: Any, **kwargs: Any):
+        super().__init__(*args, **kwargs)
+        # TODO use constant
+        self._attribute = ATTR_MODE
+        self._device_class = DEVICE_TYPE_HUB_MODE
+
+    @property
+    def options(self) -> List[str]:
+        return self._hub.modes or []
+
+    async def async_select_option(self, option: str) -> None:
+        """Change the selected option."""
+        await self._hub.set_mode(option)
+
+
+async def async_setup_entry(
+    hass: HomeAssistant,
+    entry: ConfigEntry,
+    async_add_entities: EntityAdder,
+) -> None:
+    """Initialize selectors devices."""
+
+    # If the hub supports modes, add a select for it
+    hub = get_hub(hass, entry.entry_id)
+    if hub.mode_supported:
+        hub_entities = [HubitatModeSelect(hub=hub, device=hub.device)]
+        hub.add_entities(hub_entities)
+        async_add_entities(hub_entities)

custom_components/hubitat/sensor.py
@@ -35,8 +35,14 @@ from homeassistant.const import (
 from homeassistant.core import HomeAssistant
 from homeassistant.util import dt as dt_util
 
-from .const import TEMP_F
-from .device import HubitatEntity
+from .const import (
+    ATTR_HSM_STATUS,
+    ATTR_MODE,
+    DEVICE_TYPE_HUB_HSM_STATUS,
+    DEVICE_TYPE_HUB_MODE,
+    TEMP_F,
+)
+from .device import HubitatEntity, get_hub
 from .entities import create_and_add_entities
 from .types import EntityAdder
 
@@ -47,6 +53,7 @@ class HubitatSensor(HubitatEntity):
     """A generic Hubitat sensor."""
 
     _attribute: str
+    _attribute_name: Optional[str]
     _units: str
     _device_class: Optional[str]
 
@@ -58,7 +65,10 @@ class HubitatSensor(HubitatEntity):
     @property
     def name(self) -> str:
         """Return this sensor's display name."""
-        return f"{super().name} {self._attribute}"
+        attr_name = getattr(self, "_attribute_name", None) or self._attribute.replace(
+            "_", " "
+        )
+        return f"{super().name} {attr_name}"
 
     @property
     def state(self) -> Union[float, int, str, None]:
@@ -227,6 +237,31 @@ class HubitatUpdateSensor(HubitatEntity):
         return False
 
 
+class HubitatHsmSensor(HubitatSensor):
+    """
+    A sensor that reports a hub's HSM status.
+    """
+
+    def __init__(self, *args: Any, **kwargs: Any):
+        """Initialize an hsm status sensor."""
+        super().__init__(*args, **kwargs)
+        self._attribute = ATTR_HSM_STATUS
+        self._device_class = DEVICE_TYPE_HUB_HSM_STATUS
+        self._attribute_name = "HSM status"
+
+
+class HubitatHubModeSensor(HubitatSensor):
+    """
+    A sensor that reports a hub's mode.
+    """
+
+    def __init__(self, *args: Any, **kwargs: Any):
+        """Initialize an hsm status sensor."""
+        super().__init__(*args, **kwargs)
+        self._attribute = ATTR_MODE
+        self._device_class = DEVICE_TYPE_HUB_MODE
+
+
 _SENSOR_ATTRS: Tuple[Tuple[str, Type[HubitatSensor]], ...] = (
     (ATTR_BATTERY, HubitatBatterySensor),
     (ATTR_HUMIDITY, HubitatHumiditySensor),
@@ -253,6 +288,8 @@ async def async_setup_entry(
 ) -> None:
     """Initialize sensor devices."""
 
+    add_hub_entities(hass, entry, async_add_entities)
+
     # Add an update sensor for every device
     await create_and_add_entities(
         hass, entry, async_add_entities, "sensor", HubitatUpdateSensor, is_update_sensor
@@ -268,3 +305,22 @@ async def async_setup_entry(
         await create_and_add_entities(
             hass, entry, async_add_entities, "sensor", attr[1], is_sensor
         )
+
+
+def add_hub_entities(
+    hass: HomeAssistant, entry: ConfigEntry, async_add_entities: EntityAdder
+) -> None:
+    """Add entities for hub services."""
+
+    hub_entities = []
+    hub = get_hub(hass, entry.entry_id)
+
+    if hub.hsm_supported:
+        hub_entities.append(HubitatHsmSensor(hub=hub, device=hub.device))
+
+    if hub.mode_supported:
+        hub_entities.append(HubitatHubModeSensor(hub=hub, device=hub.device))
+
+    if len(hub_entities) > 0:
+        hub.add_entities(hub_entities)
+        async_add_entities(hub_entities)

custom_components/hubitat/services.py
@@ -1,3 +1,4 @@
+from logging import getLogger
 from typing import Union, cast
 
 from custom_components.hubitat.device import HubitatEntity, get_hub
@@ -13,7 +14,9 @@ from .const import (
     ATTR_ARGUMENTS,
     ATTR_CODE,
     ATTR_DELAY,
+    ATTR_HUB,
     ATTR_LENGTH,
+    ATTR_MODE,
     ATTR_NAME,
     ATTR_POSITION,
     DOMAIN,
@@ -23,8 +26,12 @@ from .const import (
     SERVICE_SET_CODE_LENGTH,
     SERVICE_SET_ENTRY_DELAY,
     SERVICE_SET_EXIT_DELAY,
+    SERVICE_SET_HSM,
+    SERVICE_SET_HUB_MODE,
 )
 from .lock import HubitatLock
+
+_LOGGER = getLogger(__name__)
 
 CLEAR_CODE_SCHEMA = vol.Schema(
     {vol.Required(ATTR_ENTITY_ID): cv.entity_id, vol.Required(ATTR_POSITION): int}
@@ -49,6 +56,12 @@ SET_CODE_SCHEMA = vol.Schema(
 )
 SET_DELAY_SCHEMA = vol.Schema(
     {vol.Required(ATTR_ENTITY_ID): cv.entity_id, vol.Required(ATTR_DELAY): int}
+)
+SET_HSM_SCHEMA = vol.Schema(
+    {vol.Required(ATTR_COMMAND): str, vol.Optional(ATTR_HUB): str}
+)
+SET_HUB_MODE_SCHEMA = vol.Schema(
+    {vol.Required(ATTR_MODE): str, vol.Optional(ATTR_HUB): str}
 )
 
 
@@ -103,6 +116,38 @@ def async_register_services(
         delay = cast(int, service.data.get(ATTR_LENGTH))
         await entity.set_exit_delay(delay)
 
+    async def set_hsm(service: ServiceCall) -> None:
+        target_hub = hub
+        if ATTR_HUB in service.data:
+            hub_id = cast(str, service.data.get(ATTR_HUB)).lower()
+            found_hub = False
+            for _hub in hass.data[DOMAIN].values():
+                if _hub.id == hub_id:
+                    found_hub = True
+                    target_hub = _hub
+            if not found_hub:
+                _LOGGER.error("Could not find a hub with ID %s", hub_id)
+                return
+
+        command = cast(str, service.data.get(ATTR_COMMAND))
+        await target_hub.set_hsm(command)
+
+    async def set_hub_mode(service: ServiceCall) -> None:
+        target_hub = hub
+        if ATTR_HUB in service.data:
+            hub_id = cast(str, service.data.get(ATTR_HUB)).lower()
+            found_hub = False
+            for _hub in hass.data[DOMAIN].values():
+                if _hub.id == hub_id:
+                    found_hub = True
+                    target_hub = _hub
+            if not found_hub:
+                _LOGGER.error("Could not find a hub with ID %s", hub_id)
+                return
+
+        mode = cast(str, service.data.get(ATTR_MODE))
+        await target_hub.set_mode(mode)
+
     hass.services.async_register(
         DOMAIN, SERVICE_CLEAR_CODE, clear_code, schema=CLEAR_CODE_SCHEMA
     )
@@ -124,6 +169,12 @@ def async_register_services(
     hass.services.async_register(
         DOMAIN, SERVICE_SET_EXIT_DELAY, set_exit_delay, schema=SET_DELAY_SCHEMA
     )
+    hass.services.async_register(
+        DOMAIN, SERVICE_SET_HSM, set_hsm, schema=SET_HSM_SCHEMA
+    )
+    hass.services.async_register(
+        DOMAIN, SERVICE_SET_HUB_MODE, set_hub_mode, schema=SET_HUB_MODE_SCHEMA
+    )
 
 
 def async_remove_services(hass: HomeAssistant, config_entry: ConfigEntry) -> None:
@@ -133,3 +184,5 @@ def async_remove_services(hass: HomeAssistant, config_entry: ConfigEntry) -> Non
     hass.services.async_remove(DOMAIN, SERVICE_SET_ENTRY_DELAY)
     hass.services.async_remove(DOMAIN, SERVICE_SET_EXIT_DELAY)
     hass.services.async_remove(DOMAIN, SERVICE_SEND_COMMAND)
+    hass.services.async_remove(DOMAIN, SERVICE_SET_HSM)
+    hass.services.async_remove(DOMAIN, SERVICE_SET_HUB_MODE)

custom_components/hubitat/services.yaml
@@ -98,3 +98,47 @@ send_command:
         values
       selector:
         text:
+
+set_hsm:
+  description: Arm or disarm a hub’s HSM
+  fields:
+    hub:
+      name: Hub ID
+      description: >-
+        The ID of a hub (first 8 digits of the Maker API token). This is only
+        needed if you have multiple hubs.
+      example: ab12cd34
+      selector: 
+        text:
+    command:
+      name: Command
+      description: The HSM command to send
+      required: true
+      selector:
+        select:
+          options:
+            - 'armAway'
+            - 'armHome'
+            - 'armNight'
+            - 'disarm'
+            - 'disarmAll'
+
+set_hub_mode:
+  description: Set a hub’s mode
+  fields:
+    hub:
+      name: Hub ID
+      description: >-
+        The ID of a hub (first 8 digits of the Maker API token). This is only
+        needed if you have multiple hubs.
+      example: ab12cd34
+      selector:
+        text:
+    mode:
+      name: Mode
+      description: >-
+        The desired mode. Typical values are Day, Night, and Away, but your hub
+        may support other values.
+      required: true
+      selector:
+        text:

pyproject.toml
@@ -33,4 +33,3 @@ testpaths = ["tests"]
 [build-system]
 requires = ["poetry>=0.12"]
 build-backend = "poetry.masonry.api"
-

commit e46e61d8aeb634d372410bc6d933eed675bcb5a4

Use device name for devices rather than entity name
This fixes a regression introduced in ac6c9e8c

resolves #133

custom_components/hubitat/device.py
@@ -44,9 +44,9 @@ class HubitatBase(Removable):
             "identifiers": {(DOMAIN, self.device_id)},
         }
 
-        # if this entities device isn't the hub, link it to the hub
+        # if this entity's device isn't the hub, link it to the hub
         if self.device_id != self._hub.id:
-            info["name"] = self.name
+            info["name"] = self._device.name
             info["via_device"] = ((DOMAIN, self._hub.id),)
             info["model"] = self.type
             info["manufacturer"] = "Hubitat"