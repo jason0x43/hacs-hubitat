4b08149f4e7bace531803de6349202d526539e03
Jason Cheatham <jason@jasoncheatham.com>
2024-07-12 14:32:05 -0600

    Valve implementation improvements
    * Add supported feature flags
    * Use 'open' and 'closed' states rather than 'opening' and 'closing'
    * Type cleanup

--- custom_components/hubitat/valve.py
+++ custom_components/hubitat/valve.py
@@ -2,10 +2,14 @@
 
 import re
 from logging import getLogger
-from typing import TYPE_CHECKING, Any, Unpack
+from typing import TYPE_CHECKING, Unpack
 
 from custom_components.hubitat.hubitatmaker.const import DeviceAttribute
-from homeassistant.components.valve import ValveDeviceClass, ValveEntity
+from homeassistant.components.valve import (
+    ValveDeviceClass,
+    ValveEntity,
+    ValveEntityFeature,
+)
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
@@ -35,26 +39,35 @@ class HubitatValve(HubitatEntity, ValveEntity):
             else ValveDeviceClass.WATER
         )
         self._attr_unique_id = f"{super().unique_id}::valve"
+        self._attr_supported_features = (
+            ValveEntityFeature.OPEN
+            | ValveEntityFeature.CLOSE
+        )
         self.load_state()
 
     def load_state(self):
-        self._attr_is_opening = self._get_is_open()
+        self._attr_is_open = self._get_is_open()
+        self._attr_is_closed = self._get_is_closed()
 
     def _get_is_open(self) -> bool:
         """Return True if the valve is open."""
         return self.get_str_attr(DeviceAttribute.VALVE) == "open"
+
+    def _get_is_closed(self) -> bool:
+        """Return True if the valve is open."""
+        return self.get_str_attr(DeviceAttribute.VALVE) == "closed"
 
     @property
     def device_attrs(self) -> tuple[DeviceAttribute, ...] | None:
         """Return this entity's associated attributes"""
         return (DeviceAttribute.VALVE,)
 
-    async def async_open_valve(self, **kwargs: Any) -> None:
+    async def async_open_valve(self) -> None:
         """Open the valve."""
-        _LOGGER.debug(f"Opening {self.name} with {kwargs}")
+        _LOGGER.debug(f"Opening {self.name}")
         await self.send_command(DeviceCommand.OPEN)
 
-    async def async_close_valve(self, **kwargs: Any) -> None:
+    async def async_close_valve(self) -> None:
         """Close the valve."""
         _LOGGER.debug(f"Closing {self.name}")
         await self.send_command(DeviceCommand.CLOSE)
